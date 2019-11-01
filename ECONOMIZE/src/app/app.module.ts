3742a66e5b521b6a5b3598891ebdd5c5016380d1
luccatenorio <lucca.tenorio9@gmail.com>
2019-10-31 22:37:18 -0300

    criei os 3 componentes (gastos, metas e despesas) que serão necessários para o desenvolvimento do nosso projeto

--- ECONOMIZE/src/app/app.module.ts
+++ ECONOMIZE/src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { GastosComponent } from './gastos/gastos.component';
+import { DespesasComponent } from './despesas/despesas.component';
+import { MetasComponent } from './metas/metas.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GastosComponent,
+    DespesasComponent,
+    MetasComponent
   ],
   imports: [
     BrowserModule
